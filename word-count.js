542d097830f78a3b255c41053cf85e27c8d822b1
Heath Robbins <heathrobbins89@gmail.com>
2023-06-02 12:44:48 -0400

    Create word-count.js

---
+++ word-count.js
@@ -0,0 +1,24 @@
+function wordCount(string) {
+   var array = string.split(" ");
+   for (i = 0; i < array.length; i++) {
+       console.log(array[i]);
+   }
+   array.sort();
+   var current = null; 
+   var count = 0;
+   for (var i = 0; i < array.length; i++) {
+        if (array[i] != current) {
+             if (count > 0) {
+                 console.log(current + " comes out " + count + " times ");
+             } 
+             current = array[i];
+             count = 1;
+        } else {
+             count++;
+        }
+   }
+   if (count > 0) {
+       console.log(current + " comes " + count + " times ");
+   }
+}
+wordCount("a man a plan a canal panama");
